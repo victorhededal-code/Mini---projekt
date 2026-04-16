91b2454752013744218ea13fe5ac841e6c4a09e2
GlaZEyE02 <patrick02vj@gmail.com>
2026-04-16 11:27:23 +0200

    Tilføjet alle races

--- lib/scr/race.py
+++ lib/scr/race.py
@@ -1,4 +1,4 @@
-from race_dicts import human_dict, dwarf_dict
+from race_dicts import aasimar_dict, dragonborn_dict, dwarf_dict, elf_dict, gnome_dict, goliath_dict, halfling_dict, human_dict, orc_dict, tiefling_dict,
 
 class Race:
     def __init__(self, name, data):
@@ -10,13 +10,11 @@ class Race:
         print(f"RACE: {self.name.upper()}")
         print("=" * 50)
 
-        # --- Basic info ---
         creature_type = self.data.get("CreatureType") or self.data.get("Creature Type")
         speed = self.data.get("Speed")
 
         print(f"{'Type':<12}: {creature_type}")
 
-        # Print all size types (Small, Medium, etc.)
         print("Size(s)     :")
         for key in ["Small", "Medium", "Large"]:
             if key in self.data:
@@ -24,10 +22,8 @@ class Race:
 
         print(f"{'Speed':<12}: {speed}")
 
-        # --- Features ---
         print("\nFeatures:")
 
-        # keys that are NOT features
         ignore_keys = ["CreatureType", "Creature Type", "Speed", "Small", "Medium", "Large"]
 
         i = 1
@@ -40,10 +36,8 @@ class Race:
         print("=" * 50)
 
 
-# Create races
 human = Race("Human", human_dict)
 dwarf = Race("Dwarf", dwarf_dict)
 
-# Print sheets
 human.print_sheet()
 dwarf.print_sheet()
