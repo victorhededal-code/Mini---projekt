ae0ea0e7d03f7df206af64b24553a2ec8f52c4dd
magnusthostrup <magnus.thostrup@gmail.com>
2026-04-22 10:16:47 +0200

    app.py og html fixed - no clue hvad problemet var

--- lib/scr/app.py
+++ lib/scr/app.py
@@ -1,24 +1,15 @@
 from flask import Flask, render_template, redirect, url_for, request
 
-# pip install Flask
 app = Flask(__name__)
 
+
 @app.route("/")
-def index():
-    return render_template("index.html")
+def landing():
+    return render_template("index.html")   
 
-@app.route("/create", methods =['POST', 'GET'])
-def create_character():
-    render_template('/create_character.html')
-
-@app.route("/saved_characters", methods =['POST', 'GET'])
-def saved_characters():
-    render_template('/saved_characters.html')
-
-@app.route("/about_us", methods =['POST', 'GET'])
-def about_us():
-    render_template('/about_us.html')
-
+@app.route("/create", methods=['POST', 'GET']) 
+def create():
+    return render_template("create_character.html")
 
 
 app.run(debug=True)
