287e7854b313bd3c62b3ceb116eb060d831e5af8
victor <victorhededal@gmail.com>
2026-04-22 11:20:15 +0200

    fine tuning character create

--- lib/scr/creation_character/character_creation.py
+++ lib/scr/creation_character/character_creation.py
@@ -1,9 +1,10 @@
-from lib.scr.creation_character.background import Background
-from lib.scr.creation_character.prof.profession_dicts import *
-from lib.scr.creation_character.StatRoller import *
+from background import Background
+from profession_dicts import *
+from StatRoller import *
 from race_dicts import *
 from asiapplier import *
-def character_create(character_list:list) -> list:
+from features import *
+def character_create(character_list:dict) -> dict:
     "goes through a character creation process"
     while True:
         character = {}
@@ -16,8 +17,20 @@ def character_create(character_list:list) -> list:
         proff = proff.lower()
         lvl=int(input('choose level'))
         character['LVL:']=lvl
+        if lvl >= 4:
+            lvl4opt=input("Would you like to \n 1. Pick a feat \n 2. Upgradabe ilityscore \n Pick a number: ")
 
-        for x in range(1, lvl + 1):
+            if lvl4opt == "1":
+                for key in featuredict:
+                    print(key)
+                featopt=input("Choose feature")
+            elif lvl4opt == "2":
+
+            else:
+                print("error")
+                pass
+
+        for x in range(1, +-lvl + 1):
             for i in proffesion[proff][x]:
                 print(i, ':', proffesion[proff][x][i])
         race=input('choose Race / Species: \nHuman\n elf\n gnome\n a fucking dragon')
@@ -34,11 +47,12 @@ def character_create(character_list:list) -> list:
         align = input('Choose your characters alignment here')
         character['Alignment:'] = align
         character['Stats:\n'] = character_stats
-        check = input('finished?')
         for i in character:
             print(i, character[i])
-        character_list.append(character)
-        if check == 'yes' or check == 'y' or check == 'Yes' or check == 'YES' or check == 'Y' or check == '':
+        character_list[name]=character
+        check = input('finished?')
+        check=check.lower()
+        if check == 'yes' or check == 'y' or check == '':
             return character_list
 character_list = []
 character_list =character_create(character_list)
--- lib/scr/creation_character/features.py
+++ lib/scr/creation_character/features.py
@@ -1,4 +1,6 @@
-feature={"Savage Attack": "You've trained to deal particularly damaging strikes. Once per turn when you hit a target with a weapon, you can roll the weapons damage dice twice and use either roll against the target.",
+featuredict={"Savage Attack": "You've trained to deal particularly damaging strikes. Once per turn when you hit a target with a weapon, you can roll the weapons damage dice twice and use either roll against the target.",
          "Tough": 2
          ""
          }
+
+optfeat={}
