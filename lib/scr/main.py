833ae1e8f99c92e581626bd01cbaf13f301500f9
GlaZEyE02 <patrick02vj@gmail.com>
2026-04-29 10:42:56 +0200

    Going back

--- lib/scr/main.py
+++ lib/scr/main.py
@@ -1,11 +1,4 @@
 from menu import menu
-import json
+while True:
+    menu()
 
-def reset_json(file_path="characters.json"):
-    with open(file_path, "w") as f:
-        json.dump([], f)
-
-if __name__ == "__main__":
-    reset_json()
-    while True:
-        menu()
